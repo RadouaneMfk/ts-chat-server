50f4d94b9c6ebf6d6218ac198b8a5272a7818df9
radouane mouafik <red1@kali.kali>
2026-04-05 20:02:25 +0100

    done

--- src/handler/room.handler.ts
+++ src/handler/room.handler.ts
@@ -1,12 +1,22 @@
-import { Socket } from "socket.io";
+import { Server, Socket } from "socket.io";
 import { getRoomMessages } from "../services/room.service";
 
-export async function joinRoom(socket: Socket, roomId: number) {
-    socket.join(roomId.toString());
-    socket.emit("messagesHistory", await getRoomMessages(roomId));
+async function joinRoom(socket: Socket, roomId: string) {
+    socket.join(roomId);
+    socket.emit("messagesHistory", await getRoomMessages(parseInt(roomId)));
     socket.to(roomId.toString()).emit("userJoined", socket.data.user);
 }
 
-export function leaveRoom(socket: Socket, roomId: number) {
-    
+function leaveRoom(socket: Socket, roomId: string) {
+    socket.leave(roomId);
+    socket.to(roomId).emit("userLeft", socket.data.user);
+}
+
+export function roomHandler(io: Server, socket: Socket) {
+    socket.on("joinRoom", async (roomId: string) => {
+        await joinRoom(socket, roomId);
+    });
+    socket.on("leaveRoom", (roomId: string) => {
+        leaveRoom(socket, roomId);
+    });
 }
